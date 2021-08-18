9d776711b853695933830a9c99a2be267655af33
Simon Kurz <simon.kurz@rocketmail.com>
2021-08-18 20:46:46 +0200

    Removed adding pip installs to path from fish configh

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,10 +2,5 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-# Set environment variables
-
-# Include pip installs
-set -ax PATH ~/.local/bin
-
 # Show system information
 neofetch
